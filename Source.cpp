0f77e076839b802a2ac6b826edeffcec22058ec9
Carmen DiGironimo <digiro77@rowan.edu>
2019-01-22 19:33:11 -0500

    I changed the name and email.

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,8 @@
 
 
 /***********************************************
-* Name: Takeshwari Kamal
-* Email: kamalt4@students.rowan.edu
+* Name: Carmen DiGironimo
+* Email: digiro77@students.rowan.edu
 ***********************************************
 * Purpose:
 *  This program calculates area of circle
